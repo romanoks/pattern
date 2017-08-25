729bde17b3b1b5130a5bba8c04f1a694253ea785
raks <werwolf959@gmail.com>
2017-08-25 10:09:31 +0300

    interpreter

---
+++ src/interpreter/OrExpression.java
@@ -0,0 +1,16 @@
+package interpreter;
+
+public class OrExpression implements Expression {
+    Expression exp1 = null;
+    Expression exp2 = null;
+
+    public OrExpression (Expression exp1, Expression exp2){
+        this.exp1 = exp1;
+        this.exp2 = exp2;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+        return exp1.interpret(context) || exp2.interpret(context);
+    }
+}
